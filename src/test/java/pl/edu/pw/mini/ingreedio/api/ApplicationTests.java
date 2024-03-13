69bb009fa22c4ba0a86d4fd1917a5d20ebd58125
KuZu <jakubzuchowski13@gmail.com>
2024-03-13 23:44:23 +0100

    init

---
+++ src/test/java/pl/edu/pw/mini/ingreedio/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.mini.ingreedio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
