705e28d9d47138fa2ef0929526153b4a9f05efe6
GitQueenZofia <zofia.anna.grabowska@gmail.com>
2024-03-20 19:54:21 +0100

    Basic endpoints for products and users

---
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/controller/UserController.java
@@ -0,0 +1,40 @@
+package pl.edu.pw.mini.ingreedio.api.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.pw.mini.ingreedio.api.model.User;
+import pl.edu.pw.mini.ingreedio.api.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllProducts() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getProductById(@PathVariable Integer id) {
+        User user = userService.getUserById(id);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping
+    public ResponseEntity<User> addProduct(@RequestBody User user) {
+        User savedUser = userService.addUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/model/UserProducts.java
@@ -0,0 +1,7 @@
+package pl.edu.pw.mini.ingreedio.api.model;
+
+public class UserProducts {
+    private Integer id;
+    private Integer userId;
+    private Integer productId;
+}
