9bc8dac4492e5edef973a1e5f0e10e619286c617
Michał Okurowski <migoxdesign@gmail.com>
2024-04-12 11:57:22 +0200

    Fixed imports

--- src/main/java/pl/edu/pw/mini/ingreedio/api/model/Role.java
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/model/Role.java
@@ -1,11 +1,16 @@
 package pl.edu.pw.mini.ingreedio.api.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Set;
-
 
 @Getter
 @Setter
--- src/main/java/pl/edu/pw/mini/ingreedio/api/model/RolePermission.java
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/model/RolePermission.java
@@ -1,6 +1,16 @@
 package pl.edu.pw.mini.ingreedio.api.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import lombok.Getter;
 import lombok.Setter;
 
