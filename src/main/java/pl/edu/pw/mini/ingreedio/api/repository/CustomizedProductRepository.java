c783fd7c9b0b80e71f585997729cddda4060a2e4
GitQueenZofia <zofia.anna.grabowska@gmail.com>
2024-04-27 19:49:12 +0200

    Products paging

--- src/main/java/pl/edu/pw/mini/ingreedio/api/repository/CustomizedProductRepository.java
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/repository/CustomizedProductRepository.java
@@ -1,9 +1,11 @@
 package pl.edu.pw.mini.ingreedio.api.repository;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import pl.edu.pw.mini.ingreedio.api.criteria.ProductFilterCriteria;
-import pl.edu.pw.mini.ingreedio.api.dto.ProductDto;
+import pl.edu.pw.mini.ingreedio.api.model.Product;
 
 public interface CustomizedProductRepository {
-    List<ProductDto> getProductsMatching(ProductFilterCriteria criteria);
+    Page<Product> getProductsMatching(ProductFilterCriteria criteria, Pageable pageable);
 }
