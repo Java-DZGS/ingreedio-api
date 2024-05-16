8d5e114bd5f0dc78afce9a95c9b7d2273b69cde9
kubazuch <jakubzuchowski13@gmail.com>
2024-05-16 11:56:30 +0200

    This is my last straw.

--- src/test/java/pl/edu/pw/mini/ingreedio/api/IntegrationTest.java
+++ src/test/java/pl/edu/pw/mini/ingreedio/api/IntegrationTest.java
@@ -1,75 +1,71 @@
 package pl.edu.pw.mini.ingreedio.api;
 
-import java.io.File;
-import java.time.Duration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Transactional
 public class IntegrationTest {
-    private static final String POSTGRES_NAME = "postgres_1";
+    private static final String POSTGRES_NAME = "postgres:13.1-alpine";
     private static final int POSTGRES_PORT = 5432;
     private static final String POSTGRES_JDBC = "jdbc:postgresql://%s:%s/%s";
     private static final String POSTGRES_DB = "ingreedio";
     private static final String POSTGRES_USERNAME = "compose-postgres";
     private static final String POSTGRES_PASSWORD = "compose-postgres";
 
-    private static final String[] MONGO_NAME = new String[] {"mongo1_1", "mongo2_1", "mongo3_1"};
+    private static final String MONGO_NAME = "mongo:latest";
     public static final int MONGO_PORT = 27017;
     private static final String MONGO_DB = "ingreedio";
-    private static final String MONGO_OPTIONS = "?replicaSet=rs0";
-    private static final String MONGO_URI = "mongodb://%s/%s";
+    private static final String MONGO_OPTIONS = "?replicaSet=docker-rs&directConnection=true";
+    private static final String MONGO_URI = "mongodb://%s:%d/%s";
 
-    @SuppressWarnings("rawtypes")
-    public static DockerComposeContainer environment =
-        new DockerComposeContainer(new File("src/test/resources/compose-test.yml"))
-            .withExposedService(POSTGRES_NAME, POSTGRES_PORT,
-                Wait.forListeningPort().withStartupTimeout(
-                    Duration.ofSeconds(30)))
-            .withExposedService(MONGO_NAME[0], MONGO_PORT,
-                Wait.forListeningPort().withStartupTimeout(
-                    Duration.ofSeconds(30)))
-            .withExposedService(MONGO_NAME[1], MONGO_PORT + 1,
-                Wait.forListeningPort().withStartupTimeout(
-                    Duration.ofSeconds(30)))
-            .withExposedService(MONGO_NAME[2], MONGO_PORT + 2,
-                Wait.forListeningPort().withStartupTimeout(
-                    Duration.ofSeconds(30)));
+    public static PostgreSQLContainer<?> postgresContainer;
+    public static MongoDBContainer mongoContainer;
 
     @BeforeAll
     public static void setUp() {
-        environment.start();
+        postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse(POSTGRES_NAME))
+            .withExposedPorts(5432)
+            .withNetworkAliases("postgres")
+            .withUsername(POSTGRES_USERNAME)
+            .withPassword(POSTGRES_PASSWORD)
+            .withDatabaseName(POSTGRES_DB);
+
+        mongoContainer = new MongoDBContainer(DockerImageName.parse(MONGO_NAME))
+            .withNetworkAliases("mongo")
+            .withExposedPorts(MONGO_PORT);
+
+        postgresContainer.start();
+        mongoContainer.start();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        mongoContainer.stop();
+        postgresContainer.stop();
     }
 
     @DynamicPropertySource
     public static void overrideProperties(DynamicPropertyRegistry registry) {
         // JPA -- POSTGRES
         registry.add("spring.datasource.url", () -> String.format(POSTGRES_JDBC,
-            environment.getServiceHost(POSTGRES_NAME, POSTGRES_PORT),
-            environment.getServicePort(POSTGRES_NAME, POSTGRES_PORT),
+            postgresContainer.getHost(),
+            postgresContainer.getMappedPort(POSTGRES_PORT),
             POSTGRES_DB));
         registry.add("spring.datasource.username", () -> POSTGRES_USERNAME);
         registry.add("spring.datasource.password", () -> POSTGRES_PASSWORD);
 
         // MONGO
-        var host = environment.getServiceHost(MONGO_NAME[0], MONGO_PORT);
-        var port = environment.getServicePort(MONGO_NAME[0], MONGO_PORT);
-        var connectionBuilder = new StringBuilder(host).append(':').append(port);
-
-        for (int i = 1; i < 3; i++) {
-            host = environment.getServiceHost(MONGO_NAME[i], MONGO_PORT + i);
-            port = environment.getServicePort(MONGO_NAME[i], MONGO_PORT + i);
-            connectionBuilder.append(',').append(host).append(':').append(port);
-        }
-
-        String connection = String.format(MONGO_URI, connectionBuilder, MONGO_OPTIONS);
-        System.out.println(connection);
+        String connection = String.format(MONGO_URI,
+            mongoContainer.getHost(), mongoContainer.getMappedPort(MONGO_PORT),
+            MONGO_OPTIONS);
         registry.add("spring.data.mongodb.uri", () -> connection);
         registry.add("spring.data.mongodb.database", () -> MONGO_DB);
     }
