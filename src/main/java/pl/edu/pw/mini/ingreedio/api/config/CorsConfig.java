ad1948dd47bacc0d312cdd9ea26b51fbfe3d6f93
kubazuch <jakubzuchowski13@gmail.com>
2024-05-21 22:59:13 +0200

    Remote frontend CORS

--- src/main/java/pl/edu/pw/mini/ingreedio/api/config/CorsConfig.java
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.mini.ingreedio.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.frontend.url:https://example.org}")
+    private String frontendUri;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-                // todo: change to cloud link when frontend is deployed
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000", frontendUri);
             }
         };
     }
