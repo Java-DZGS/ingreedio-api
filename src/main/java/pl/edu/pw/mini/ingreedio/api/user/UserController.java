9c3c1b8924dbb2ed1535132977894cc094524b0f
Marcin Słupczyński <marcinxslup@gmail.com>
2024-05-23 20:17:17 +0200

    Endpoint rename

--- src/main/java/pl/edu/pw/mini/ingreedio/api/user/UserController.java
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/user/UserController.java
@@ -71,7 +71,7 @@ public class UserController {
     }
 
     @Operation(security = {@SecurityRequirement(name = "Bearer Authentication")})
-    @GetMapping("/ratings")
+    @GetMapping("/reviews")
     public ResponseEntity<List<ReviewDto>> getUserRatings() {
         Optional<User> userOptional = userService
             .getUserByUsername(authService.getCurrentUsername());
