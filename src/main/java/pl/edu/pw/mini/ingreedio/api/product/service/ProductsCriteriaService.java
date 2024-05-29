96babafa5e30855136a8c171983a5724abebb79e
Marcin Słupczyński <132074948+mslup@users.noreply.github.com>
2024-05-29 15:49:43 +0200

    Added brand, category, provider to FilterCriteria (#109)

--- src/main/java/pl/edu/pw/mini/ingreedio/api/product/service/ProductsCriteriaService.java
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/product/service/ProductsCriteriaService.java
@@ -8,62 +8,99 @@ import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import pl.edu.pw.mini.ingreedio.api.brand.model.Brand;
+import pl.edu.pw.mini.ingreedio.api.brand.service.BrandService;
+import pl.edu.pw.mini.ingreedio.api.category.model.Category;
+import pl.edu.pw.mini.ingreedio.api.category.service.CategoryService;
 import pl.edu.pw.mini.ingreedio.api.ingredient.dto.IngredientDto;
 import pl.edu.pw.mini.ingreedio.api.ingredient.service.IngredientService;
 import pl.edu.pw.mini.ingreedio.api.product.criteria.ProductCriteria;
 import pl.edu.pw.mini.ingreedio.api.product.criteria.ProductsSortingCriteria;
+import pl.edu.pw.mini.ingreedio.api.provider.model.Provider;
+import pl.edu.pw.mini.ingreedio.api.provider.service.ProviderService;
 
 @Service
 @AllArgsConstructor
 public class ProductsCriteriaService {
     private final IngredientService ingredientService;
+    private final ProviderService providerService;
+    private final BrandService brandService;
+    private final CategoryService categoryService;
 
     public ProductCriteria getProductsCriteria(Optional<Set<Long>> ingredientsToExclude,
                                                Optional<Set<Long>> ingredientsToInclude,
                                                Optional<Integer> minRating,
                                                Optional<String> phrase,
                                                Optional<List<String>> sortBy,
-                                               Optional<Boolean> liked) {
+                                               Optional<Boolean> liked,
+                                               Optional<Set<Long>> providers,
+                                               Optional<Set<Long>> brandsToExclude,
+                                               Optional<Set<Long>> brandsToInclude,
+                                               Optional<Set<Long>> categories) {
 
-        // TODO: provider, brand, category (add arguments too)
         var builder = ProductCriteria.builder();
         builder.hasMatchScoreSortCriteria(false);
 
         ingredientsToExclude.ifPresent(
-            ingredients -> {
-                builder.ingredientsNamesToExclude(ingredientService
-                    .getIngredientsByIds(ingredients)
-                    .stream()
-                    .map(IngredientDto::name)
-                    .collect(Collectors.toSet()));
-            });
+            ingredients -> builder.ingredientsNamesToExclude(ingredientService
+                .getIngredientsByIds(ingredients)
+                .stream()
+                .map(IngredientDto::name)
+                .collect(Collectors.toSet())));
 
         ingredientsToInclude.ifPresent(
-            ingredients -> {
-                builder.ingredientsNamesToInclude(ingredientService
-                    .getIngredientsByIds(ingredients)
-                    .stream()
-                    .map(IngredientDto::name)
-                    .collect(Collectors.toSet()));
-            });
+            ingredients -> builder.ingredientsNamesToInclude(ingredientService
+                .getIngredientsByIds(ingredients)
+                .stream()
+                .map(IngredientDto::name)
+                .collect(Collectors.toSet())));
 
         minRating.ifPresent(builder::minRating);
 
         phrase.ifPresent(phraseString ->
             builder.phraseKeywords(Arrays.stream(
-                phraseString.replaceAll("%20+", " ")
-                    .trim()
-                    .split(" "))
+                    phraseString.replaceAll("%20+", " ")
+                        .trim()
+                        .split(" "))
                 .collect(Collectors.toSet())));
 
         liked.ifPresent(builder::liked);
 
+        providers.ifPresent(
+            providerIds -> builder.providersNames(providerService
+                .getProvidersByIds(providerIds)
+                .stream()
+                .map(Provider::getName)
+                .collect(Collectors.toSet())));
+
+        brandsToExclude.ifPresent(
+            brandIds -> builder.brandsNamesToExclude(brandService
+                .getBrandsByIds(brandIds)
+                .stream()
+                .map(Brand::getName)
+                .collect(Collectors.toSet())));
+
+        brandsToInclude.ifPresent(
+            brandIds -> builder.brandsNamesToInclude(brandService
+                .getBrandsByIds(brandIds)
+                .stream()
+                .map(Brand::getName)
+                .collect(Collectors.toSet())));
+
+        categories.ifPresent(
+            categoryIds -> builder.categoriesNames(categoryService
+                .getCategoriesByIds(categoryIds)
+                .stream()
+                .map(Category::getName)
+                .collect(Collectors.toSet()))
+        );
+
         sortBy.ifPresent(sortingSignatures -> {
             List<ProductsSortingCriteria> sortingCriteriaList =
                 sortingSignatures.stream()
-                .map(this::getProductsSortingCriteria)
-                .flatMap(Optional::stream)
-                .toList();
+                    .map(this::getProductsSortingCriteria)
+                    .flatMap(Optional::stream)
+                    .toList();
 
             Optional<ProductsSortingCriteria> foundCriteria = sortingCriteriaList.stream()
                 .filter(criteria -> criteria.byField().equals("matchScore"))
@@ -72,7 +109,6 @@ public class ProductsCriteriaService {
             builder.sortingCriteria(sortingCriteriaList);
             builder.hasMatchScoreSortCriteria(foundCriteria.isPresent());
         });
-
 
         return builder.build();
     }
