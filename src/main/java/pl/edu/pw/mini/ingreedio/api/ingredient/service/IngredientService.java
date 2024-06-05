3dbce7092ce9c4a689b1bf749b70e506bf79b270
Marcin Słupczyński <marcinxslup@gmail.com>
2024-06-05 16:39:34 +0200

    Refactor - eaten

--- src/main/java/pl/edu/pw/mini/ingreedio/api/ingredient/service/IngredientService.java
+++ src/main/java/pl/edu/pw/mini/ingreedio/api/ingredient/service/IngredientService.java
@@ -1,18 +1,15 @@
 package pl.edu.pw.mini.ingreedio.api.ingredient.service;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.edu.pw.mini.ingreedio.api.auth.service.AuthService;
-import pl.edu.pw.mini.ingreedio.api.ingredient.dto.IngredientDto;
-import pl.edu.pw.mini.ingreedio.api.ingredient.mapper.IngredientDtoMapper;
 import pl.edu.pw.mini.ingreedio.api.ingredient.model.Ingredient;
 import pl.edu.pw.mini.ingreedio.api.ingredient.repository.IngredientRepository;
+import pl.edu.pw.mini.ingreedio.api.product.exception.IngredientNotFoundException;
 import pl.edu.pw.mini.ingreedio.api.user.model.User;
 import pl.edu.pw.mini.ingreedio.api.user.service.UserService;
 
@@ -21,34 +18,33 @@ import pl.edu.pw.mini.ingreedio.api.user.service.UserService;
 public class IngredientService {
     private final IngredientRepository ingredientRepository;
     private final UserService userService;
-    private final AuthService authService;
-    private final IngredientDtoMapper ingredientDtoMapper;
 
     @Transactional(readOnly = true)
-    public Optional<IngredientDto> getIngredientById(Long id) {
-        return ingredientRepository.findById(id).map(ingredientDtoMapper);
-    }
-
-    @Transactional(readOnly = true)
-    public Set<IngredientDto> getIngredientsByIds(Set<Long> ids) {
-        return ingredientRepository.findAllByIdIn(ids).stream()
-            .map(ingredientDtoMapper)
-            .collect(Collectors.toSet());
-    }
-
-    @Transactional(readOnly = true)
-    public List<IngredientDto> getIngredients(int count, String queryString, User user,
-                                              boolean skipAllergens) {
+    public List<Ingredient> getIngredients(int count, String queryString, User user,
+                                           boolean skipAllergens) {
         String[] query = queryString.split("\\s+");
-        return ingredientRepository.findIngredientsMatchingQuery(PageRequest.of(0, count),
-            query, user, skipAllergens).map(ingredientDtoMapper).toList();
+        return ingredientRepository.findIngredientsMatchingQuery(
+                PageRequest.of(0, count), query, user, skipAllergens)
+            .toList();
     }
 
     @Transactional(readOnly = true)
-    public List<IngredientDto> getIngredients(int count, String queryString) {
+    public List<Ingredient> getIngredients(int count, String queryString) {
         String[] query = queryString.split("\\s+");
-        return ingredientRepository.findIngredientsMatchingQuery(PageRequest.of(0, count), query)
-            .map(ingredientDtoMapper).toList();
+        return ingredientRepository.findIngredientsMatchingQuery(
+                PageRequest.of(0, count), query)
+            .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public Ingredient getIngredientById(long id) throws IngredientNotFoundException {
+        return ingredientRepository.findById(id)
+            .orElseThrow(() -> new IngredientNotFoundException(id));
+    }
+
+    @Transactional(readOnly = true)
+    public Set<Ingredient> getIngredientsByIds(Set<Long> ids) {
+        return new HashSet<>(ingredientRepository.findAllByIdIn(ids));
     }
 
     @Transactional
@@ -57,88 +53,44 @@ public class IngredientService {
     }
 
     @Transactional(readOnly = true)
-    public List<IngredientDto> getLikedIngredients() {
-        Optional<User> userOptional = userService
-            .getUserByUsername(authService.getCurrentUsername());
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            return user.getLikedIngredients().stream().map(ingredientDtoMapper).toList();
-        }
-        return List.of();
+    public List<Ingredient> getLikedIngredients(User user) {
+        return user.getLikedIngredients().stream().toList();
     }
 
     @Transactional(readOnly = true)
-    public List<IngredientDto> getAllergens() {
-        Optional<User> userOptional = userService
-            .getUserByUsername(authService.getCurrentUsername());
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            return user.getAllergens().stream().map(ingredientDtoMapper).toList();
-        }
-        return List.of();
+    public List<Ingredient> getAllergens(User user) {
+        return user.getAllergens().stream().toList();
     }
 
     @Transactional
-    public boolean likeIngredient(Long id) {
-        Optional<User> userOptional = userService
-            .getUserByUsername(authService.getCurrentUsername());
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            Optional<Ingredient> ingredientOptional = ingredientRepository.findById(id);
-            if (ingredientOptional.isPresent()) {
-                Ingredient ingredient = ingredientOptional.get();
-                userService.likeIngredient(user, ingredient);
-                return true;
-            }
-        }
-        return false;
+    public void likeIngredient(long id, User user) throws IngredientNotFoundException {
+        Ingredient ingredient = ingredientRepository.findById(id)
+            .orElseThrow(() -> new IngredientNotFoundException(id));
+
+        userService.likeIngredient(user, ingredient);
     }
 
     @Transactional
-    public boolean unlikeIngredient(Long id) {
-        Optional<User> userOptional = userService
-            .getUserByUsername(authService.getCurrentUsername());
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            Optional<Ingredient> ingredientOptional = ingredientRepository.findById(id);
-            if (ingredientOptional.isPresent()) {
-                Ingredient ingredient = ingredientOptional.get();
-                userService.unlikeIngredient(user, ingredient);
-                return true;
-            }
-        }
-        return false;
+    public void unlikeIngredient(long id, User user) throws IngredientNotFoundException {
+        Ingredient ingredient = ingredientRepository.findById(id)
+            .orElseThrow(() -> new IngredientNotFoundException(id));
+
+        userService.unlikeIngredient(user, ingredient);
     }
 
     @Transactional
-    public boolean addAllergen(Long id) {
-        Optional<User> userOptional = userService
-            .getUserByUsername(authService.getCurrentUsername());
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            Optional<Ingredient> ingredientOptional = ingredientRepository.findById(id);
-            if (ingredientOptional.isPresent()) {
-                Ingredient ingredient = ingredientOptional.get();
-                userService.addAllergen(user, ingredient);
-                return true;
-            }
-        }
-        return false;
+    public void addAllergen(long id, User user) throws IngredientNotFoundException {
+        Ingredient ingredient = ingredientRepository.findById(id)
+            .orElseThrow(() -> new IngredientNotFoundException(id));
+
+        userService.addAllergen(user, ingredient);
     }
 
     @Transactional
-    public boolean removeAllergen(Long id) {
-        Optional<User> userOptional = userService
-            .getUserByUsername(authService.getCurrentUsername());
-        if (userOptional.isPresent()) {
-            User user = userOptional.get();
-            Optional<Ingredient> ingredientOptional = ingredientRepository.findById(id);
-            if (ingredientOptional.isPresent()) {
-                Ingredient ingredient = ingredientOptional.get();
-                userService.removeAllergen(user, ingredient);
-                return true;
-            }
-        }
-        return false;
+    public void removeAllergen(long id, User user) throws IngredientNotFoundException {
+        Ingredient ingredient = ingredientRepository.findById(id)
+            .orElseThrow(() -> new IngredientNotFoundException(id));
+
+        userService.removeAllergen(user, ingredient);
     }
 }
