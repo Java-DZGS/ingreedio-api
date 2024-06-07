02c24a403ccfce0e3eace2152e5d929513c5bb40
KuZu <jakubzuchowski13@gmail.com>
2024-06-07 06:31:38 +0200

    fix

--- src/test/java/pl/edu/pw/mini/ingreedio/api/TestConfiguration.java
+++ src/test/java/pl/edu/pw/mini/ingreedio/api/TestConfiguration.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.transaction.annotation.Transactional;
 import pl.edu.pw.mini.ingreedio.api.user.model.User;
 import pl.edu.pw.mini.ingreedio.api.user.service.UserService;
 
 @Configuration
+@Transactional
 public class TestConfiguration {
     @Autowired
     private UserService userService;
